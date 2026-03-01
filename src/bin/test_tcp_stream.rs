59790f6983a8f015578a35dc0a6157d4738ce6da
KevinDev64 <kevindev56@yandex.ru>
2026-03-01 19:52:09 +0300

    feat: ECHOOO answer checker

--- src/bin/test_tcp_stream.rs
+++ src/bin/test_tcp_stream.rs
@@ -25,6 +25,27 @@ fn main() {
 	println!("Data -> {:?}", data_buffer);
 	stream.write_all(data_buffer).expect("Failed to send data!");
 
+	let mut header_buffer  = [0_u8; 12];
+	stream.read(&mut header_buffer).expect("Failed to receive ECHOOO answer!");
+	let message_type = str::from_utf8(&header_buffer[2..8]).expect("Wrong message type in answer!");
+	let mut raw_length = vec![0_u8; 4];
+	raw_length[..].copy_from_slice(&header_buffer[8..12]);
+	let received_length = u32::from_be_bytes(raw_length.as_array().unwrap().to_owned());
+	if (message_type == "ECHOOO") && (received_length == length) {
+		println!("ECHOOO header is correct!")
+	} else {
+		eprintln!("BAD HEADER SECTION!");
+	}
+
+	let mut data_buffer = vec![0_u8; received_length.try_into().unwrap()];
+    stream.read(&mut data_buffer).expect("Failed to receive data section!");
+    let data = str::from_utf8(&mut data_buffer).to_owned().unwrap();
+	if data == "data" {
+		println!("Data section is correct!");
+	} else {
+		eprintln!("BAD DATA SECTION!");
+	}
+
 	let abort_buffer = b"\0\0ABORTT\0\0\0\0";
 	stream.write_all(abort_buffer).expect("Failed to send ABORTT command!");
 
