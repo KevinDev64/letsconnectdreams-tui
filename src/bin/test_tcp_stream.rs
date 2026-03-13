51bfb162192623c14627fff99a55524212f2915f
KevinDev64 <kevindev56@yandex.ru>
2026-03-14 02:36:38 +0300

    feat: test encrypted messaging with server

--- src/bin/test_tcp_stream.rs
+++ src/bin/test_tcp_stream.rs
@@ -1,5 +1,8 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
+use rsa::pkcs1::{DecodeRsaPrivateKey, DecodeRsaPublicKey, EncodeRsaPrivateKey, EncodeRsaPublicKey};
+use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
+use rand::rngs::OsRng;
 
 fn main() {
 	let mut stream = TcpStream::connect("127.0.0.1:4222")
@@ -45,6 +48,69 @@ fn main() {
 	} else {
 		eprintln!("BAD DATA SECTION!");
 	}
+
+	let mut rng = OsRng;
+    let private_key = RsaPrivateKey::new(&mut rng, 3072)
+        .expect("Failed to generate RSA keypair!");
+    let public_key = RsaPublicKey::from(&private_key);
+	let public_key_string = public_key.to_pkcs1_pem(rsa::pkcs8::LineEnding::LF).unwrap();
+	let public_key_raw = public_key_string.as_bytes();
+	let length = (public_key_raw.len() as u32).to_be_bytes();
+	let mut header_buffer = [0_u8; 12];
+	header_buffer[2..8].copy_from_slice(b"PUBKEY");
+	header_buffer[8..12].copy_from_slice(&length);
+	stream.write_all(&header_buffer).unwrap();
+	stream.write_all(public_key_raw).unwrap();
+
+	let mut header_buffer = [0_u8; 12];
+	header_buffer[2..8].copy_from_slice(b"PUBSRV");
+	stream.write_all(&header_buffer).expect("Failed to send PUBSRV command!");
+
+	let mut header_buffer = [0_u8; 12];
+	stream.read(&mut header_buffer).expect("Failed to read header after requesting server's pubkey!");
+	if !(&header_buffer[2..8] == b"PUBKEY") {
+		panic!("Wrong answer from SERVER when trying to get server's pubkey!");
+	}
+	let mut raw_length = vec![0_u8; 4];
+	raw_length[..].copy_from_slice(&header_buffer[8..12]);
+	let length = u32::from_be_bytes(raw_length.as_array().unwrap().to_owned());
+	let mut data_buffer = vec![0_u8; length.try_into().unwrap()];
+	stream.read(&mut data_buffer).expect("Failed to read data section!");
+	let server_pub_key = str::from_utf8(&data_buffer).unwrap();
+	let server_pub_key = RsaPublicKey::from_pkcs1_pem(server_pub_key).unwrap();
+	println!("YES! Server's public key: {:?}", server_pub_key);
+
+	let mut header_buffer = [0_u8; 12];
+	header_buffer[2..8].copy_from_slice(b"HELLOO");
+	let data = String::from("I'm client!");
+	let data = data.as_bytes();
+	let mut rng = OsRng;
+    let data = server_pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, data)
+        .expect("Failed to encrypt message!");
+	let length = data.len() as u32;
+	let length_raw = length.to_be_bytes();
+	header_buffer[8..12].copy_from_slice(&length_raw);
+
+	stream.write_all(&header_buffer).expect("Failed to send HELLOO!");
+	stream.write_all(&data).expect("Failed to send HELLOO data!");
+
+	let mut header_buffer = [0_u8; 12];
+	stream.read(&mut header_buffer).expect("Failed to read HELLOO answer header!");
+	if !(&header_buffer[2..8] == b"HELLOO") {
+		panic!("Wrong answer from SERVER when trying to get HELLOO answer!");
+	}
+	let mut raw_length = vec![0_u8; 4];
+	raw_length[..].copy_from_slice(&header_buffer[8..12]);
+	let length = u32::from_be_bytes(raw_length.as_array().unwrap().to_owned());
+	let mut data_buffer = vec![0_u8; length.try_into().unwrap()];
+	stream.read(&mut data_buffer).expect("Failed to read data section!");
+	let decrypted = private_key.decrypt(Pkcs1v15Encrypt, &data_buffer)
+        .expect("Failed to decrypt message!");	
+	let decrypted = str::from_utf8(&decrypted).unwrap();
+	if !(decrypted == "I'm server!") {
+		panic!("Wrong HELLOO message from server!");
+	}
+	println!("HELLOO message from SERVER: {}", decrypted);
 
 	let abort_buffer = b"\0\0ABORTT\0\0\0\0";
 	stream.write_all(abort_buffer).expect("Failed to send ABORTT command!");
